6841cd75cbb61b0583f678c8bfc2ca3dc98c9620
zaidkhalailahjo <zaidkhalailahjo@gmail.com>
2026-06-16 17:38:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,26 @@
- 
+// Basic Service Worker
+const CACHE_NAME = 'ds-pwa-cache-v1';
+const urlsToCache = [
+  '/'
+];
 
 self.addEventListener('install', event => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    return caches.delete(cacheName); // حذف أي كاش قديم
-                })
-            );
-        }).then(() => self.clients.claim())
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 self.addEventListener('fetch', event => {
-    // جلب الملفات من الإنترنت دائماً ولا نستخدم الكاش أبداً
-    event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
